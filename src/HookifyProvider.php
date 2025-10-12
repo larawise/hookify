76c4cc2aaf1a1473a733280071ba832fab7a853d
selcukcukur <selcukcukur@outlook.com.tr>
2025-10-12 20:09:05 +0300

    A service provider has been created for `Hookify`.

---
+++ src/HookifyProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Larawise\Hookify;
+
+use Larawise\Packagify\Packagify;
+use Larawise\Packagify\PackagifyProvider;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Hookify
+ * @version     v1.0.0
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ */
+class HookifyProvider extends PackagifyProvider
+{
+    /**
+     * Configure the packagify package.
+     *
+     * @param Packagify $package
+     *
+     * @return void
+     */
+    public function configure(Packagify $package)
+    {
+        // Set the packagify package name.
+        $package->name('hookify');
+
+        // Set the packagify package description.
+        $package->description('A fluent, taggable, testable hook system for Laravel. Register listeners with clarity, fire actions with precision, and filter data with confidence — all in one elegant package.');
+    }
+}
